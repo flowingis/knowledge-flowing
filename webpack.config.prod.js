439a497687755d0e50330927b48d1162a9603493
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2018-09-28 17:36:15 +0200

    using env variables in production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,9 +1,18 @@
 const commonConfiguration = require('./webpack.config.common')
-
-console.log('TEST', process.env.GOGGLE_CLIENT_ID)
+const webpack = require('webpack')
 
 module.exports = {
   ...commonConfiguration,
+  plugins: [
+    ...commonConfiguration.plugins,
+    new webpack.DefinePlugin({
+      GOGGLE_CLIENT_ID: JSON.stringify(process.env.GOGGLE_CLIENT_ID),
+      GOGGLE_API_KEY: JSON.stringify(process.env.GOGGLE_API_KEY),
+      PIPEDRIVE_API_TOKEN: JSON.stringify(process.env.PIPEDRIVE_API_TOKEN),
+      PIPEDRIVE_COMPANY_DOMAIN: JSON.stringify('ideato'),
+      BASE_GOOGLE_DRIVE_DIRECTORY: JSON.stringify('1WZS3_nShJv')
+    })
+  ],
   mode: 'production',
   devtool: 'none'
 }
